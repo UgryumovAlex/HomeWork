118d51d81aa1bc7bcea83a37112e38fd27300f0b
a.ugryumov <ugryumov_alex@mail.ru>
2021-03-20 14:29:50 +0300

    Домашняя работа № 2, сделано 1 задание

--- src/HomeWork.java
+++ src/HomeWork.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * 1. Напишите метод, на вход которого подаётся двумерный строковый массив размером 4х4.
  *    При подаче массива другого размера необходимо бросить исключение MyArraySizeException.
@@ -15,7 +17,39 @@
  * */
 
 public class HomeWork {
+
     public static void main(String[] args) {
         System.out.println("Java, уровень 2, домашняя работа №2");
+
+        String[][] strArr = {
+                              {"Весна", "Лето", "Осень", "Зима"},
+                              {"Азия", "Европа", "Америка", "Африка"},
+                              {"Футбол", "Хоккей", "Баскетбол", "Воллейбол"},
+                              {"Конь", "Слон", "Ладья", "Ферзь"}
+                             };
+        try {
+            int arrayRes = calcArray(strArr);
+            System.out.println(arrayRes);
+        } catch (MyArraySizeException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+
+    private static int calcArray(String[][] strArr) throws MyArraySizeException {
+        //Проверяем размерность входящего массива
+        if (strArr.length != 4) {
+            throw new MyArraySizeException("Количество строк массива не равно 4");
+        } else {
+            for (int i=0; i < strArr.length; i++) {
+                if (strArr[i].length != 4) {
+                    throw new MyArraySizeException("Количество столбцов строки " + (i+1) + " не равно 4");
+                }
+            }
+        }
+        System.out.println("Размерность массива корректная");
+
+        return 0; //Пока подсчёт элементов не реализован
     }
 }
